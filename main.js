87892af11afbcfa5126f3aabc60ba2f09ad8b8f0
YounisCodeer <80555260+YounisCodeer@users.noreply.github.com>
2022-04-09 23:13:35 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup() {
+    canvas = createCanvas(250, 250);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
